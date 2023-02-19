83a704a80bd0a61660df10ccdf8f9eab06ca9100
Jose Ederango Jr <joseederangojr@Macbook-Pro-2018.local>
2023-02-19 16:17:35 +0800

    naive implementation of observer pattern

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,119 @@
 /**
- * Some predefined delay values (in milliseconds).
+ * Observer Pattern
  */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
+
+
+interface Observable<T> {
+  subscribe(observer: Observer<T>);
+  unsubscribe(observer: Observer<T>);
+  notify(data: T);
 }
 
-/**
- * Returns a Promise<string> that resolves after a given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - A number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
+interface Observer<T> {
+  watch(data: T);
 }
 
-// Please see the comment in the .eslintrc.json file about the suppressed rule!
-// Below is an example of how to use ESLint errors suppression. You can read more
-// at https://eslint.org/docs/latest/user-guide/configuring/rules#disabling-rules
+class Cart {
+  private items: Item[] = [];
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export async function greeter(name: any) { // eslint-disable-line @typescript-eslint/no-explicit-any
-  // The name parameter should be of type string. Any is used only to trigger the rule.
-  return await delayedHello(name, Delays.Long);
+  add(item: Item) {
+    this.items.push(item)
+  }
+
+  remove(item: Item) {
+    this.items = this.items.filter($item => $item === item)
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  getTotal() {
+    return this.items.reduce((total, item) => total + item.price,0)
+  }
+
+  getItemCount() {
+    return this.items.length
+  }
 }
+
+class Item {
+  public id: number;
+  public name: string;
+  public price: number;
+
+  constructor(name: string, price: number) {
+    this.id = Date.now()
+    this.name = name;
+    this.price = price;
+  }
+}
+
+class CartObserver implements Observer<Cart> {
+  private callback;
+  constructor(callback: (data: Cart) => void) {
+    this.callback = callback
+  }
+
+  watch(data: Cart) {
+    this.callback(data)
+  }
+}
+
+class CartObservable implements Observable<Cart> {
+  private observers: Observer<Cart>[] = [];
+  private cart: Cart = new Cart()
+
+  subscribe(observer: Observer<Cart>) {
+    this.observers.push(observer)
+  }
+
+  unsubscribe(observer: Observer<Cart>) {
+    this.observers = this.observers.filter(obs => obs !== observer)
+  }
+
+  notify() {
+    this.observers.forEach(observer => {
+      observer.watch(this.cart);
+    })
+  }
+
+  add(item: Item) {
+    this.cart.add(item)
+    this.notify()
+  }
+
+  remove(item: Item) {
+    this.cart.remove(item)
+    this.notify()
+  }
+}
+
+
+
+const cartObservable = new CartObservable()
+
+const totalPriceObserver = new CartObserver(cart => {
+  console.log('Total Price:', cart.getTotal())
+})
+const totalCountObserver = new CartObserver(cart => {
+  console.log('Total count: ', cart.getItemCount())
+})
+const listItemsObserver = new CartObserver(cart => {
+  cart.getItems()
+  .forEach(item => {
+    console.log('Name:', item.name, '----', 'Price: ', item.price)
+  })
+})
+
+// Subscribe
+cartObservable.subscribe(listItemsObserver)
+cartObservable.subscribe(totalCountObserver)
+cartObservable.subscribe(totalPriceObserver)
+
+cartObservable.add(new Item('Item 1', 10))
+cartObservable.add(new Item('Item 2', 15))
+cartObservable.add(new Item('Item 3', 25))
+
+cartObservable.unsubscribe(totalCountObserver)
+cartObservable.add(new Item('Item 4', 50))
